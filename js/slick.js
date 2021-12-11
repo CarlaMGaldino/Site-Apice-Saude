59761e423f09479b2dc8d37c427744f0e4682104
CarlaMGaldino <carlinhamg20@gmail.com>
2021-12-11 13:01:54 -0300

    primeiro commit

---
+++ js/slick.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+  $("#slide-hospital").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+$(".banner-home__conteudo-slide-top").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$(".hospital-conteudo__home-slide-itens").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: "hospital-conteudo__home-slide-g",
+});
+$(".hospital-conteudo__home-slide-g").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".hospital-conteudo__home-slide-itens",
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+});
